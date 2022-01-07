a86470699dd2ca2026e1fb37df1de3aa0386d090
wileyj <2847772+wileyj@users.noreply.github.com>
2022-01-07 12:12:46 -0800

    up1

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-# vim:sw=4:ts=4:et
-
-set -e
 
 echo "Checking for env var API_SERVICE_NAME"
 if [ ! -z "${API_SERVICE_NAME}" ]; then
     echo "Editing nginx config file for %%API_SERVICE_NAME%%, replace with ${API_SERVICE_NAME}"
     sed -i -e "s|%%API_SERVICE_NAME%%|${API_SERVICE_NAME}|g" /etc/nginx/http.d/default.conf
 fi
+echo "Starting runit"
 runsvdir /etc/service
 
 # echo "Start default /docker-entrypoint.sh"
