23361824837604e38189e603041bd1cd95a5b133
wileyj <2847772+wileyj@users.noreply.github.com>
2022-01-07 11:06:26 -0800

    moar greasy

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+echo "Checking for env var API_SERVICE_NAME"
 if [ ! -z "${API_SERVICE_NAME}" ]; then
+    echo "Editing nginx config file for %%API_SERVICE_NAME%%, replace with ${API_SERVICE_NAME}"
     sed -i -e "s|%%API_SERVICE_NAME%%|${API_SERVICE_NAME}|g" /etc/nginx/http.d/default.conf 
 fi
-
+echo "Start default /docker-entrypoint.sh"
 /docker-entrypoint.sh
