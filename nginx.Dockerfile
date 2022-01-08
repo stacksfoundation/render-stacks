38dd88680d66d01a4eaa569566bf44b1b6ebac54
wileyj <2847772+wileyj@users.noreply.github.com>
2022-01-07 20:51:35 -0800

    modify nginx again

--- nginx.Dockerfile
+++ nginx.Dockerfile
@@ -1,24 +1,5 @@
 FROM nginx:alpine
-COPY configs/nginx.conf /etc/nginx/templates/default.conf.template
-# COPY scripts/nginx.sh /entrypoint.sh
-# RUN chmod 755 /entrypoint.sh
-# CMD ["/entrypoint.sh", "nginx", "-g", "daemon off;"]
-
-# FROM alpine
-# ARG STACKS_SVC_DIR=/etc/service
-# COPY configs/nginx.conf /etc/nginx/http.d/default.conf
-# COPY scripts/entrypoint.sh /docker-entrypoint.sh
-# COPY unit-files/run/nginx ${STACKS_SVC_DIR}/nginx/run
-
-# RUN apk add \
-#     nginx \
-#     runit \
-#     && mkdir -p \
-#     ${STACKS_SVC_DIR}/nginx \
-#     && chmod 755 \
-#     /docker-entrypoint.sh \
-#     ${STACKS_SVC_DIR}/nginx/run
-
-
-# EXPOSE 80
-# CMD /docker-entrypoint.sh
+COPY configs/nginx.conf /srv/default.conf
+COPY scripts/nginx.sh /entrypoint.sh
+RUN chmod 755 /entrypoint.sh
+CMD ["/entrypoint.sh"]
--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,21 +1,49 @@
-#!/usr/bin/env sh
-# export SERVICE_NAME=$(nslookup $STACKS_BLOCKCHAIN_API_HOST | grep Name | awk '{print $2}')
-export SERVICE_NAME=$STACKS_BLOCKCHAIN_API_HOST
-export SERVICE_PORT=$STACKS_BLOCKCHAIN_API_PORT
-export NAMESERVER=$(cat /etc/resolv.conf | grep "nameserver" | head -n1 | awk '{print $2}')
-# if [ -f "/tmp/default.conf" ]; then
-#     sed -i -e "s|\$SERVICE_NAME|$SERVICE_NAME|g" /tmp/default.conf
-#     sed -i -e "s|\$SERVICE_PORT|$SERVICE_PORT|g" /tmp/default.conf
-#     sed -i -e "s|\$NAMESERVER|$NAMESERVER|g" /tmp/default.conf
-#     mv /tmp/default.conf /etc/nginx/conf.d/default.conf
-# fi
-/docker-entrypoint.sh "$@"
+#!/bin/sh
+## copied from nginx:alpine startup script
+# vim:sw=4:ts=4:et
 
-# nginx -s reload
+set -e
 
+if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+    exec 3>&1
+else
+    exec 3>/dev/null
+fi
 
-# echo "Checking for env var API_SERVICE_NAME"
-# if [ ! -z "${API_SERVICE_NAME}" ]; then
-#     echo "Editing nginx config file for %%API_SERVICE_NAME%%, replace with ${API_SERVICE_NAME}"
-#     sed -i -e "s|%%API_SERVICE_NAME%%|${API_SERVICE_NAME}|g" /etc/nginx/conf.d/default.conf
-# fi
+if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
+    echo >&3 "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
+
+    echo >&3 "$0: Looking for shell scripts in /docker-entrypoint.d/"
+    find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
+        case "$f" in
+            *.sh)
+                if [ -x "$f" ]; then
+                    echo >&3 "$0: Launching $f";
+                    "$f"
+                else
+                    # warn on shell scripts without exec bit
+                    echo >&3 "$0: Ignoring $f, not executable";
+                fi
+                ;;
+            *) echo >&3 "$0: Ignoring $f";;
+        esac
+    done
+
+    echo >&3 "$0: Configuration complete; ready for start up"
+else
+    echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+fi
+## end copy
+
+echo >&3 "Starting nginx"
+nginx
+echo >&3 "sleeping before reload"
+sleep 10
+sed -i -e "s|\$STACKS_BLOCKCHAIN_API_HOST|$STACKS_BLOCKCHAIN_API_HOST|g" /srv/default.conf
+sed -i -e "s|\$STACKS_BLOCKCHAIN_API_PORT|$STACKS_BLOCKCHAIN_API_PORT|g" /srv/default.conf
+sleep 10
+echo >&3 "copy new config"
+cp /srv/default.conf /etc/nginx/conf.d/default.conf 
+echo >&3 "Reloading nginx"
+nginx -s reload -g "daemon off;"
+
