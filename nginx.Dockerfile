d728be211df22862833f1fce6cd4642f6cdf7d63
wileyj <2847772+wileyj@users.noreply.github.com>
2022-01-07 18:04:48 -0800

    change1

--- nginx.Dockerfile
+++ nginx.Dockerfile
@@ -1,12 +1,10 @@
 FROM nginx:alpine
-ARG API_DOMAIN
-ENV API_DOMAIN=${API_DOMAIN}
+COPY configs/nginx.conf.template /etc/nginx/templates/default.conf.template
+COPY scripts/nginx.sh /entrypoint.sh
+RUN chmod 755 /entrypoint.sh
 
-COPY configs/nginx.conf /etc/nginx/conf.d/default.conf
-COPY scripts/nginx.sh /docker-entrypoint.d/40-update-proxy.sh
-RUN chmod 755 /docker-entrypoint.d/40-update-proxy.sh
-
-EXPOSE 80
+ENTRYPOINT [ "/entrypoint.sh" ]
+CMD ["nginx", "-g", "daemon off;"]
 
 # FROM alpine
 # ARG STACKS_SVC_DIR=/etc/service
--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env sh
+export SERVICE_FQDN=$(nslookup $SERVICE_NAME | grep Name | awk '{print $2}')
+export SERVICE_PORT=$STACKS_BLOCKCHAIN_API_PORT
+export NAMESERVER=$(cat /etc/resolv.conf | grep "nameserver" | head -n1 | awk '{print $2}')
 
-echo "Checking for env var API_SERVICE_NAME"
-if [ ! -z "${API_SERVICE_NAME}" ]; then
-    echo "Editing nginx config file for %%API_SERVICE_NAME%%, replace with ${API_SERVICE_NAME}"
-    sed -i -e "s|%%API_SERVICE_NAME%%|${API_SERVICE_NAME}|g" /etc/nginx/conf.d/default.conf
-fi
-# echo "Starting runit"
-# runsvdir /etc/service
+/docker-entrypoint.sh "$@"
 
-# echo "Start default /docker-entrypoint.sh"
-# /docker-entrypoint.sh nginx
+
+# echo "Checking for env var API_SERVICE_NAME"
+# if [ ! -z "${API_SERVICE_NAME}" ]; then
+#     echo "Editing nginx config file for %%API_SERVICE_NAME%%, replace with ${API_SERVICE_NAME}"
+#     sed -i -e "s|%%API_SERVICE_NAME%%|${API_SERVICE_NAME}|g" /etc/nginx/conf.d/default.conf
+# fi
