393a2f4edc0e53ab5e37a63b34678ca1b034184a
wileyj <2847772+wileyj@users.noreply.github.com>
2022-01-07 20:26:14 -0800

    asdf2

--- nginx.Dockerfile
+++ nginx.Dockerfile
@@ -1,8 +1,8 @@
 FROM nginx:alpine
-COPY configs/nginx.conf /etc/nginx/conf.d/default.conf
-COPY scripts/nginx.sh /entrypoint.sh
-RUN chmod 755 /entrypoint.sh
-CMD ["/entrypoint.sh", "nginx", "-g", "daemon off;"]
+COPY configs/nginx.conf /etc/nginx/templates/default.conf.template
+# COPY scripts/nginx.sh /entrypoint.sh
+# RUN chmod 755 /entrypoint.sh
+# CMD ["/entrypoint.sh", "nginx", "-g", "daemon off;"]
 
 # FROM alpine
 # ARG STACKS_SVC_DIR=/etc/service
--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
-export SERVICE_NAME=$(nslookup $STACKS_BLOCKCHAIN_API_HOST | grep Name | awk '{print $2}')
+# export SERVICE_NAME=$(nslookup $STACKS_BLOCKCHAIN_API_HOST | grep Name | awk '{print $2}')
+export SERVICE_NAME=$STACKS_BLOCKCHAIN_API_HOST
 export SERVICE_PORT=$STACKS_BLOCKCHAIN_API_PORT
 export NAMESERVER=$(cat /etc/resolv.conf | grep "nameserver" | head -n1 | awk '{print $2}')
-if [ -f "/tmp/default.conf" ]; then
-    sed -i -e "s|\$SERVICE_NAME|$SERVICE_NAME|g" /tmp/default.conf
-    sed -i -e "s|\$SERVICE_PORT|$SERVICE_PORT|g" /tmp/default.conf
-    sed -i -e "s|\$NAMESERVER|$NAMESERVER|g" /tmp/default.conf
-    mv /tmp/default.conf /etc/nginx/conf.d/default.conf
-fi
+# if [ -f "/tmp/default.conf" ]; then
+#     sed -i -e "s|\$SERVICE_NAME|$SERVICE_NAME|g" /tmp/default.conf
+#     sed -i -e "s|\$SERVICE_PORT|$SERVICE_PORT|g" /tmp/default.conf
+#     sed -i -e "s|\$NAMESERVER|$NAMESERVER|g" /tmp/default.conf
+#     mv /tmp/default.conf /etc/nginx/conf.d/default.conf
+# fi
 /docker-entrypoint.sh "$@"
 
 # nginx -s reload
