a68ae6d8d72dfc6c4a8344c93def70ffe7f19f15
wileyj <2847772+wileyj@users.noreply.github.com>
2022-01-11 08:38:20 -0800

    reload still broken 1

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -9,31 +9,34 @@ else
     exec 3>/dev/null
 fi
 
-if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
-    echo >&3 "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
+if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
+    if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
+        echo >&3 "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
 
-    echo >&3 "$0: Looking for shell scripts in /docker-entrypoint.d/"
-    find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
-        case "$f" in
-            *.sh)
-                if [ -x "$f" ]; then
-                    echo >&3 "$0: Launching $f";
-                    "$f"
-                else
-                    # warn on shell scripts without exec bit
-                    echo >&3 "$0: Ignoring $f, not executable";
-                fi
-                ;;
-            *) echo >&3 "$0: Ignoring $f";;
-        esac
-    done
+        echo >&3 "$0: Looking for shell scripts in /docker-entrypoint.d/"
+        find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
+            case "$f" in
+                *.sh)
+                    if [ -x "$f" ]; then
+                        echo >&3 "$0: Launching $f";
+                        "$f"
+                    else
+                        # warn on shell scripts without exec bit
+                        echo >&3 "$0: Ignoring $f, not executable";
+                    fi
+                    ;;
+                *) echo >&3 "$0: Ignoring $f";;
+            esac
+        done
 
-    echo >&3 "$0: Configuration complete; ready for start up"
-else
-    echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+        echo >&3 "$0: Configuration complete; ready for start up"
+    else
+        echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+    fi
 fi
-echo >&3 "$0: Starting Nginx"
-exec nginx
+
+exec "$@"
+
 echo >&3 "$0: Waiting for the API to come up"
 # wait for api to respond on 3999, then update config and reload
 COUNTER=0
@@ -52,7 +55,7 @@ echo >&3 "$0"
 sleep 5
 # echo >&3 "$0: Reloading nginx"
 
-# exec nginx -s reload
+# exec "$@-s reload"
 # echo >&3 "$0:###########################################"
 # ps -ef 
 # echo >&3 "$0:###########################################"
