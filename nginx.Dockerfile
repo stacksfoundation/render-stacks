f213865551a183017c9482a6ec9cc317f0535142
wileyj <2847772+wileyj@users.noreply.github.com>
2022-01-11 09:05:17 -0800

    asdf1

--- nginx.Dockerfile
+++ nginx.Dockerfile
@@ -5,4 +5,4 @@ COPY configs/nginx-default.conf /srv/nginx-default.conf
 COPY configs/nginx.conf /srv/nginx.conf
 COPY scripts/nginx.sh /entrypoint.sh
 RUN chmod 755 /entrypoint.sh
-CMD "/entrypoint.sh"
+# CMD "/entrypoint.sh"
--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -35,33 +35,35 @@ if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
     fi
 fi
 echo >&3 "$0: exec \"$@\""
-exec "$@"
+exec "nginx"
 
-# echo >&3 "$0: Waiting for the API to come up"
-# # wait for api to respond on 3999, then update config and reload
-# COUNTER=0
-# until nc -vz $STACKS_BLOCKCHAIN_API_HOST $STACKS_BLOCKCHAIN_API_PORT >/dev/null 2>&1; do
-#     COUNTER=$((COUNTER+1))
-#     echo >&3 "$0:$COUNTER) Waiting for $STACKS_BLOCKCHAIN_API_HOST:$STACKS_BLOCKCHAIN_API_PORT"
-#     sleep 30
-# done
-# echo >&3 "$0"
-# echo >&e "$0: API is reachable ($STACKS_BLOCKCHAIN_API_HOST:$STACKS_BLOCKCHAIN_API_PORT)"
-# echo >&3 "$0: Running envsubst on /srv/nginx.conf >  /etc/nginx/conf.d/default.conf"
-# envsubst < /srv/nginx.conf > /etc/nginx/conf.d/default.conf
-# cat /etc/nginx/conf.d/default.conf
-# echo >&3 "$0 Sleep 5"
-# echo >&3 "$0"
-# sleep 5
-# echo >&3 "$0: Reloading nginx"
+echo >&3 "$0: Waiting for the API to come up"
+# wait for api to respond on 3999, then update config and reload
+COUNTER=0
+until nc -vz $STACKS_BLOCKCHAIN_API_HOST $STACKS_BLOCKCHAIN_API_PORT >/dev/null 2>&1; do
+    COUNTER=$((COUNTER+1))
+    echo >&3 "$0:$COUNTER) Waiting for $STACKS_BLOCKCHAIN_API_HOST:$STACKS_BLOCKCHAIN_API_PORT"
+    sleep 30
+done
+echo >&3 "$0"
+echo >&e "$0: API is reachable ($STACKS_BLOCKCHAIN_API_HOST:$STACKS_BLOCKCHAIN_API_PORT)"
+echo >&3 "$0: Running envsubst on /srv/nginx.conf >  /etc/nginx/conf.d/default.conf"
+envsubst < /srv/nginx.conf > /etc/nginx/conf.d/default.conf
+cat /etc/nginx/conf.d/default.conf
+echo >&3 "$0 Sleep 5"
+echo >&3 "$0"
+sleep 5
+echo >&3 "$0: Reloading nginx"
 
-# exec "$@-s reload"
-# echo >&3 "$0:###########################################"
-# ps -ef 
-# echo >&3 "$0:###########################################"
-# echo >&3 "$0"
-echo >&3 "$0:Exiting"
+exec "nginx -s reload"
+echo >&3 "$0:###########################################"
+ps -ef 
+echo >&3 "$0:###########################################"
+echo >&3 "$0"
 echo >&3 "$0: ####################################"
 echo >&3 "$0: ps -ef: $(ps -ef)"
 echo >&3 "$0: ####################################"
+while [[ 2 -gt 1]]; do
+    sleep 60
+done
 
