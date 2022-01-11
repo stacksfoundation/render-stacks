1969d8cc672b5afe3f18ce4ee2a9728ed6c2c8ba
wileyj <2847772+wileyj@users.noreply.github.com>
2022-01-11 08:44:11 -0800

    reload still broken 2

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -37,22 +37,22 @@ fi
 
 exec "$@"
 
-echo >&3 "$0: Waiting for the API to come up"
-# wait for api to respond on 3999, then update config and reload
-COUNTER=0
-until nc -vz $STACKS_BLOCKCHAIN_API_HOST $STACKS_BLOCKCHAIN_API_PORT >/dev/null 2>&1; do
-    COUNTER=$((COUNTER+1))
-    echo >&3 "$0:$COUNTER) Waiting for $STACKS_BLOCKCHAIN_API_HOST:$STACKS_BLOCKCHAIN_API_PORT"
-    sleep 30
-done
-echo >&3 "$0"
-echo >&e "$0: API is reachable ($STACKS_BLOCKCHAIN_API_HOST:$STACKS_BLOCKCHAIN_API_PORT)"
-echo >&3 "$0: Running envsubst on /srv/nginx.conf >  /etc/nginx/conf.d/default.conf"
-envsubst < /srv/nginx.conf > /etc/nginx/conf.d/default.conf
-cat /etc/nginx/conf.d/default.conf
-echo >&3 "$0 Sleep 5"
-echo >&3 "$0"
-sleep 5
+# echo >&3 "$0: Waiting for the API to come up"
+# # wait for api to respond on 3999, then update config and reload
+# COUNTER=0
+# until nc -vz $STACKS_BLOCKCHAIN_API_HOST $STACKS_BLOCKCHAIN_API_PORT >/dev/null 2>&1; do
+#     COUNTER=$((COUNTER+1))
+#     echo >&3 "$0:$COUNTER) Waiting for $STACKS_BLOCKCHAIN_API_HOST:$STACKS_BLOCKCHAIN_API_PORT"
+#     sleep 30
+# done
+# echo >&3 "$0"
+# echo >&e "$0: API is reachable ($STACKS_BLOCKCHAIN_API_HOST:$STACKS_BLOCKCHAIN_API_PORT)"
+# echo >&3 "$0: Running envsubst on /srv/nginx.conf >  /etc/nginx/conf.d/default.conf"
+# envsubst < /srv/nginx.conf > /etc/nginx/conf.d/default.conf
+# cat /etc/nginx/conf.d/default.conf
+# echo >&3 "$0 Sleep 5"
+# echo >&3 "$0"
+# sleep 5
 # echo >&3 "$0: Reloading nginx"
 
 # exec "$@-s reload"
@@ -60,4 +60,9 @@ sleep 5
 # ps -ef 
 # echo >&3 "$0:###########################################"
 # echo >&3 "$0"
+echo >&3 "$0:Exiting"
+echo >&3 "$0: ####################################"
+echo >&3 "$0: ps -ef: $(ps -ef)"
+echo >&3 "$0: ####################################"
+exit
 
